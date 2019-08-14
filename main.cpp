430ef909e0b50cc4d1060d17e70624ab7cc4d42e
RahulKumar1998 <50517820+RahulKumar1998@users.noreply.github.com>
2019-08-14 12:37:51 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"enter the number";
+	cin>>num;
+	int mid,ans,end=num-1,beg=0;
+	
+	
+		
+		
+		while(beg<=end)
+		{
+			mid=(beg+end)/2;
+			if(num==(mid*mid)) 
+			{   
+				ans=mid;
+				break;
+			}		
+			else if(num>(mid*mid))
+			{
+				beg=mid+1;
+				ans=mid;
+			\
+			}
+			else
+			{
+            	end=mid-1;
+			}
+			\
+		}
+		
+		cout<<ans<<endl;
+		
+	return 0;
+}
