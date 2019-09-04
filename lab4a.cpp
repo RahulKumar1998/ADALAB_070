082864e142e0e42527a058f3d7fceb97ebec0b08
RahulKumar1998 <50517820+RahulKumar1998@users.noreply.github.com>
2019-09-04 12:35:29 +0530

    Update lab4a.cpp

--- lab4a.cpp
+++ lab4a.cpp
@@ -1,36 +1,46 @@
 #include<iostream>
 using namespace std;
-class connected{
+class connected
+{
     int adjacent[10][10],visited[10],v,n;
-public:
-        void input(){
-        cout<<"enter the no. of components\n";
-        cin>>n;
-        for (int i=0;i<n;i++){
-            visited[i]=0;
+    public:
+        void input()
+        {
+            cout<<"enter the no. of components\n";
+            cin>>n;
+            for (int i=0;i<n;i++)
+            {
+                visited[i]=0;
+            }
+            cout<<"enter the adjacent matrix of graph\n";
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    cin>>adjacent[i][j];
+                }
+            }
+   
         }
-        cout<<"enter the adjacent matrix of graph\n";
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                cin>>adjacent[i][j];
-            }}
-       // cout<<"enter the starting vertex  :";
-       // cin>>v;
 
-        }
-
-        void DFS(int v){
+        void DFS(int v)
+        {
             cout<<v+1<<",";
             visited[v]=1;
-            for (int i=0;i<n;i++){
-                if(adjacent[v][i]==1 && visited[i]==0){
+            for (int i=0;i<n;i++)
+            {
+                if(adjacent[v][i]==1 && visited[i]==0)
+                {
                     DFS(i);
                 }
             }
         }
-        void connected1(){
-            for(int j=0;j<n;j++){
-                if(visited[j]==0){
+        void connected1()
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(visited[j]==0)
+                {
                     DFS(j);
                     cout<<"\n";
                 }
@@ -40,6 +50,7 @@ public:
 
 
 };
+
 int main(){
     connected c;
     c.input();
